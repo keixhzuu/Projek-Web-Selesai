068e9081df6de88d094c0bd5ff74155aa98a77e7
Fadilah Nur Sabiyyah <154042229+faddsky@users.noreply.github.com>
2024-11-08 16:13:56 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -11,6 +11,16 @@ if (!isset($_SESSION['user_id'])) {
 $user_id = $_SESSION['user_id'];
 $id_pins = $_GET['id'];
 
+// ambil path
+$sql = "SELECT image_path FROM pins WHERE id_pins = $id_pins";
+$hasil = mysqli_query($conn, $sql);
+$data = mysqli_fetch_assoc($hasil);
+ 
+// hapus gambar di folder uploads
+if ($data && file_exists($data['image_path'])) {
+    unlink($data['image_path']);
+}
+
 // Query untuk menghapus data berdasarkan ID gambar (id_pins) dan user_id yang login
 $sql = "DELETE FROM pins WHERE id_pins = $id_pins AND id_user = $user_id";
 $hasil = mysqli_query($conn, $sql);
